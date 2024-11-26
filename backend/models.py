bbf8c5a9b48951f859bc090fd941b1b53c6f6e54
oskarolaszczyk <oskarolaszczyk@gmail.com>
2024-11-26 13:34:07 +0100

    Add model for Act and Tag

--- backend/models.py
+++ backend/models.py
@@ -22,3 +22,34 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(128), nullable=False)
     role = db.Column(db.Enum(UserRole), default=UserRole.user, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Act(db.Model):
+    __tablename__ = "acts"
+
+    act_id = db.Column(db.Integer, primary_key=True)
+    du_code = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    journal_no = db.Column(db.Integer, nullable=False, default=0)
+    num_edits = db.Column(db.Integer, nullable=False, default=0)
+    text_payload = db.Column(db.Text, nullable=False)
+    date_scraped = db.Column(db.Date, nullable=False, default=datetime.utcnow().date)
+    last_edited_date = db.Column(db.Date, nullable=True)
+    last_tag_added_date = db.Column(db.Date, nullable=True)
+    position = db.Column(db.Integer, nullable=False, default=0)  
+    part_no = db.Column(db.Integer, nullable=False, default=0)  
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow, nullable=True)
+
+
+class ActTag(db.Model):
+    __tablename__ = "acts_tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    act_id = db.Column(db.Integer, db.ForeignKey("acts.act_id"), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    assigner_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+
+    act = db.relationship("Act", backref=db.backref("tags", cascade="all, delete-orphan"))
+    assigner = db.relationship("User", backref=db.backref("assigned_tags", lazy="dynamic"))
